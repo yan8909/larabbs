bf6cff143eff703238dbdb6f533f90f4e5f7442c
yan8909 <yan8909@gmail.com>
2019-01-10 10:11:22 +0000

    make auth

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,8 +17,8 @@
         <!-- Right Side Of Navbar -->
         <ul class="navbar-nav navbar-right">
           <!-- Authentication Links -->
-          <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
         </ul>
       </div>
     </div>
