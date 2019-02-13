121705de54066cde16edea72c1c5f5e1573c8eed
yan8909 <yan8909@gmail.com>
2019-02-13 08:19:14 +0000

    SEO URL

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -23,8 +23,13 @@ class TopicsController extends Controller
         return view('topics.index', compact('topics'));
     }
 
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -40,7 +45,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
 		$topic->save();
 		
-		return redirect()->route('topics.show', $topic->id)->with('success', 'Created successfully.');
+		return redirect()->to($topic->link())->with('success', 'Created successfully.');
 	}
 
 	public function edit(Topic $topic)
@@ -55,7 +60,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('success', 'Updated successfully.');
+		return redirect()->to($topic->link())->with('success', 'Updated successfully.');
 	}
 
 	public function destroy(Topic $topic)
@@ -63,7 +68,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('success', 'Deleted successfully.');
+		return redirect()->to($topic->link())->with('success', 'Deleted successfully.');
 	}
 
 	public function uploadImage(Request $request, ImageUploadHandler $uploader)
