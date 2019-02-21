f93af8f4021c6787f755cf30a362c389001f8b8f
yan8909 <yan8909@gmail.com>
2019-02-21 04:15:08 +0000

    user can reply

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
