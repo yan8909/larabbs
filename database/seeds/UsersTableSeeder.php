b17ea323b07eba4377474580ef8e67b16fdac61a
yan8909 <43824130+yan8909@users.noreply.github.com>
2019-05-07 13:01:42 +0800

    first user be admin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -44,9 +44,9 @@ class UsersTableSeeder extends Seeder
  
          // 单独处理第一个用户的数据
          $user = User::find(1);
-         $user->name = 'jerry';
-         $user->email = 'jerry@test.com';
-         $user->password = bcrypt('jerryy');
+         $user->name = 'admin';
+         $user->email = 'admin@test.com';
+         $user->password = bcrypt('password');
          $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
          $user->save();
 
